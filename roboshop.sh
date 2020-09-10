052664b044d110deff1a0f450525cd0097a8669d
shehan567 <hushhwsply@gmail.com>
2020-09-10 13:36:21 -0400

    Added Services, and Added Print function

---
+++ roboshop.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+### Functions
+Print () {
+  echo -e "\t\t\t\e[5;1;4;34m$1\e[0m"
+}
+
+
+frontend () {
+  Print "Installing Frontend"
+}
+
+
+#mongodb
+
+
+#catalogue
+#redis
+
+
+#user
+
+
+#cart
+
+
+#mysql
+
+
+#shipping
+
+
+#rabbitmq
+
+
+#payment
+
+
+
+
+
+
+
+# Main Program
+
+
+#frontend
+#mongodb
+#catalogue
+#redis
+#user
+#cart
+#mysql
+#shipping
+#rabbitmq
+#payment
