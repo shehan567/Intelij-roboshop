fc3438ebff81a3b0e2964ed6493699afebecfba8
shehan567 <hushhwsply@gmail.com>
2020-09-15 14:40:05 -0400

    Stat_CONT Added to sed command

--- roboshop.sh
+++ roboshop.sh
@@ -150,6 +150,7 @@ frontend () {
 
   sed -i -e "s/CATALOGUE/${CATALOGUE}/" -e "s/CART/${CART}/" -e "s/USER/${USER}/" -e "s/SHIPPING/${SHIPPING}/" -e "s/PAYMENT/${PAYMENT}/" /etc/nginx/nginx.conf
 
+  Stat_CONT $? "Update DNS"
 #  sed -i "s/CATALOGUE/${CATALOGUE}" /etc/nginx/nginx.conf
 #  Stat $? "Catalogue"
 #  sed -i "s/CART/${CART}" /etc/nginx/nginx.conf
