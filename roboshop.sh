4f1a55a14538772fd67214603d8fb04d5b89b4e8
shehan567 <hushhwsply@gmail.com>
2020-09-14 21:52:17 -0400

    Added REDIS_ENDPOINT & CATALOGUE_ENDPOINT for in Node_JS function.

--- roboshop.sh
+++ roboshop.sh
@@ -86,6 +86,8 @@ Node_JS() {
   Print "Setup $1 Service"
   mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
   sed -i -e "s/MONGO_ENDPOINT/mongodb.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
+  sed -i -e "s/REDIS_ENDPOINT/redis.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
+  sed -i -e "s/CATALOGUE_ENDPOINT/catalogue.${DNS_DOMAIN_NAME}/" /etc/systemd/system/$1.service
   Print "Start $1 Service"
   systemctl daemon-reload
   systemctl enable $1
